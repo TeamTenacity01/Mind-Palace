b461dd8df0eadd0e99061bedc5072768055f0c15
Ethan <Pradhan751957@gmail.com>
2024-04-26 10:03:09 -0500

    First file upload commit

---
+++ src/routes/file_upload/+page.svelte
@@ -0,0 +1,9 @@
+// +page.svelte
+<script>
+  import { supabase } from '../../supabaseClient';
+</script>
+
+<form action="?/uploadFile" method="post" enctype="multipart/form-data">
+  <input type="file" name="file" />
+  <input type="submit" value="GO" />
+</form>
