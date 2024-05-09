5045cdfeba30b07744df3b4f31a26ea88590321d
Ethan <Pradhan751957@gmail.com>
2024-05-09 09:14:43 -0500

    Account settigs page

--- src/routes/app/file_upload/+page.svelte
+++ src/routes/app/file_upload/+page.svelte
@@ -1,7 +1,4 @@
 // +page.svelte
-<script>
-  import { supabase } from '../../supabaseClient';
-</script>
 
 <form action="?/uploadFile" method="post" enctype="multipart/form-data">
   <input type="file" name="file" />
